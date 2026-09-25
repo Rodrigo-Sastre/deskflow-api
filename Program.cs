06bd1b291d11157a5d482e3318855d0248c3cadf
Rodrigo-Sastre <ad7digitalmkt@gmail.com>
2026-09-24 21:05:00 -0300

    feat: cria ChamadoController e resolve ciclo de objetos no JSON

--- Program.cs
+++ Program.cs
@@ -2,11 +2,20 @@ using DeskFlow.API.Data;
 using Microsoft.EntityFrameworkCore;
 using DeskFlow.API.Repositories;
 using DeskFlow.API.Services;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
